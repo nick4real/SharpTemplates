6dd1a2c66db9240727c283e57c45afe2e04a87bb
Nikolaj Kurkul <67540024+nick4real@users.noreply.github.com>
2026-02-06 19:54:33 +0100

    feat: scalar support

--- templates/MicroserviceTemplate/MSTemplate.API/Program.cs
+++ templates/MicroserviceTemplate/MSTemplate.API/Program.cs
@@ -1,13 +1,31 @@
 using MSTemplate.Infrastructure;
 using MSTemplate.Application;
+#if (EnableScalarSupport)
+using Scalar.AspNetCore;
+#endif
 
 // Builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
+builder.Services.AddControllers();
 
+#if (EnableScalarSupport)
+// OpenAPI support
+builder.Services.AddOpenApi();
+
+#endif
 // Application
 var app = builder.Build();
 
+#if (EnableScalarSupport)
+if (!app.Environment.IsProduction())
+{
+    app.MapOpenApi();
+    // Map the Scalar API reference endpoint
+    app.MapScalarApiReference();
+}
+
+#endif
 app.Run();
